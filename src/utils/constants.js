d91678b77cbfdc92ca26b51191ca5aaace4ee69d
DuongTanLoc <duongtanloc824@gmail.com>
2026-03-27 10:59:29 +0700

    Fix: BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,17 @@
-export const API_ROOT = 'http://localhost:8088'
+let apiRoot = ''
+console.log('import.meta.env: ', import.meta.env)
+console.log('process.env: ', process.env)
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8088'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://joji-api-3pgt.onrender.com'
+}
+console.log('🚀 ~ apiRoot:', apiRoot)
+export const API_ROOT = apiRoot
+
+// export const API_ROOT = 'http://localhost:8088'
+// export const API_ROOT = 'https://joji-api-3pgt.onrender.com'
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
